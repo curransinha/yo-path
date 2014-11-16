5a4b6bb5f871fdb2a38006d44a069c838cad615c
Leon Zaruvinsky <leon@lzaruvinsky.com>
2014-11-16 08:14:42 -0500

    ds

--- public/script.js
+++ public/script.js
@@ -30,13 +30,16 @@ function initialize() {
 	      map: map,
 	      title: 'Plot ' + (i/2)
 	  });
-		var ih = i/2
-		totalDistance += distBetween(markerArray[ih], markerArray[ih-1])
+	  var ih = i/2
+	  totalDistance += distBetween(markerArray[ih], markerArray[ih-1])
 	}
 	markerArray.push(endPos)
 	var lengthOfMarker = markerArray.length
-		totalDistance += distBetween(markerArray[lengthOfMarker],markerArray[lengthOfMarker-1])
-		document.getElementById("travelled").innerHTML += totalDistance;
+	alert(markerArray[lengthOfMarker-1])
+	totalDistance += distBetween(markerArray[lengthOfMarker-1], markerArray[lengthOfMarker-2])
+	document.getElementById("travelled").innerHTML += totalDistance;
+	alert(totalDistance)
+	
 	new google.maps.Marker({
 	      position: endPos,
 	      map: map,
@@ -59,21 +62,30 @@ function initialize() {
 
 
 	var distBetween = function(pos1,pos2){
+		function toRadians(degree) {
+			return degree * 3.14159 / 180.0
+		}
+
 		var R = 6371; // km
-		lat1 = pos1.lat();
-		lon1 = pos1.lng();
-		lat2 = pos2.lat();
-		lon2 = pos2.lng();
-		var φ1 = lat1.toRadians();
-		var φ2 = lat2.toRadians();
-		var Δφ = (lat2-lat1).toRadians();
-		var Δλ = (lon2-lon1).toRadians();
+		console.log(pos1)
+		var lat1 = pos1.k;
+		var lon1 = pos1.B;
+		var lat2 = pos2.k;
+		var lon2 = pos2.B;
+		console.log(lat1 + " " + lon1 + " " +lat2 + " " +lon2);
+
+		var φ1 = toRadians(lat1);
+		var φ2 = toRadians(lat2);
+		var Δφ = toRadians(lat2-lat1);
+		var Δλ = toRadians(lon2-lon1);
+		console.log(φ1);
 
 		var a = Math.sin(Δφ/2) * Math.sin(Δφ/2) +
 		        Math.cos(φ1) * Math.cos(φ2) *
 		        Math.sin(Δλ/2) * Math.sin(Δλ/2);
+		console.log("a="+a);
 		var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
-
+		console.log("c="+c);
 		var d = R * c;
 		return d*3280.84;
 	}
